230e62583daffb773623b078f89c1554306d910c
muhammad muneeb ajaz <muneeb.ejaz@outlook.com>
2022-02-06 20:32:51 +0500

    update verbiage

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,6 +3,14 @@ import { Formik, Form } from 'formik';
 import { TextField } from './TextField';
 import * as Yup from 'yup';
 
+const DEFAULT_STATE = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  confirmPassword: ''
+}
+
 export const Signup = () => {
    const validate = Yup.object({  
     firstName: Yup.string()
@@ -25,13 +33,7 @@ export const Signup = () => {
   
   return (
     <Formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
-      }}
+      initialValues={DEFAULT_STATE}
       validationSchema={validate}
       onSubmit={values => {
         console.log(values)
@@ -42,9 +44,9 @@ export const Signup = () => {
           <h1 className="my-4 font-weight-bold display-4">Sign Up</h1>
           <Form>
             <TextField label="First Name" name="firstName" type="text" />
-            <TextField label="last Name" name="lastName" type="text" />
+            <TextField label="Last Name" name="lastName" type="text" />
             <TextField label="Email" name="email" type="email" />
-            <TextField label="password" name="password" type="password" />
+            <TextField label="Password" name="password" type="password" />
             <TextField label="Confirm Password" name="confirmPassword" type="password" />
             <button className="btn btn-dark mt-3" type="submit">Register</button>
             <button className="btn btn-danger mt-3 ml-3" type="reset">Reset</button>
